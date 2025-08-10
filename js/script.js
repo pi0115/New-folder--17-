2c6d85e27ff5a54ddc5bc77220e97ac0ec832dec
steph <garzas0179@gmail.com>
2025-08-09 20:51:23 -0500

    Added the api key and the url

--- js/script.js
+++ js/script.js
@@ -1,214 +1,63 @@
-/* 1. Grab the input */
+const apiKey = "qNl9lf5MHzNXDC0WgDn5Cm3Atv4mmSCc";
+let offset = 0;
+let isLoading = false;
 
 
-document.querySelector(".js-go").addEventListener("click", function () {
+function fetchGIFs(searchQuery) {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&offset=${offset}`;
 
-  var input = document.querySelector("input").value;
-  getInput(input);
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      offset += data.data.length;
+      return data.data;
+    });
+}
 
-});
-
-document.querySelector(".js-userinput").addEventListener("keyup", function (e) {
-
-  var input = document.querySelector("input").value;
-
-  // if the key ENTER is pressed then run the function...
-  if (e.which === 13) {
-    getInput(input);
-  }
-
-});
-
-/* 2. Do the data stuff with the API */
-
-function getInput(item) {
-  var query = item.split(" ").join("+");
-
-  var url = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=/* 1. Grab the input */
-
-
-  document.querySelector(".js-go").addEventListener("click", function () {
-
-    var input = document.querySelector("input").value;
-    getInput(input);
-
+function displayGIFs(gifs) {
+  const gifContainer = document.getElementById("gifContainer");
+  gifs.forEach(gif => {
+    const gifImage = document.createElement("img");
+    gifImage.src = gif.images.fixed_height.url;
+    gifContainer.appendChild(gifImage);
   });
-
-  document.querySelector(".js-userinput").addEventListener("keyup", function (e) {
-
-    var input = document.querySelector("input").value;
-
-    // if the key ENTER is pressed then run the function...
-    if (e.which === 13) {
-      getInput(input);
-    }
-
-  });
-
-  /* 2. Do the data stuff with the API */
-
-  function getInput(item) {
-    var query = item.split(" ").join("+");
-
-    var url = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key="/* 1. Grab the input */
-
-
-    document.querySelector(".js-go").addEventListener("click", function () {
-
-      var input = document.querySelector("input").value;
-      getInput(input);
-
-    });
-
-    document.querySelector(".js-userinput").addEventListener("keyup", function (e) {
-
-      var input = document.querySelector("input").value;
-
-      // if the key ENTER is pressed then run the function...
-      if (e.which === 13) {
-        getInput(input);
-      }
-
-    });
-
-    /* 2. Do the data stuff with the API */
-
-    function getInput(item) {
-      var query = item.split(" ").join("+");
-      vyKq94c
-      var url = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=qNl9lf5MHzNXDC0WgDn5Cm3Atv4mmSCc";
-
-
-
-      // AJAX Request
-      var GiphyAJAXCall = new XMLHttpRequest();
-      GiphyAJAXCall.open('GET', url);
-      GiphyAJAXCall.send();
-
-      GiphyAJAXCall.addEventListener("load", function (e) {
-
-        var data = e.target.response;
-        pushToDOM(data);
-
-      });
-
-    }
-
-
-    /* 3. Show me the GIF */
-
-    function pushToDOM(input) {
-
-      var response = JSON.parse(input);
-      var imageUrls = response.data;
-      var container = document.querySelector(".js-container");
-      var result = document.querySelector(".results");
-
-      clear(container);
-      clear(result);
-
-      var imageUrls = response.data;
-
-      imageUrls.forEach(function (image) {
-
-        var src = image.images.fixed_height.url;
-
-        result.innerHTML = src.length + " GIFs found";
-        container.innerHTML += '<a href=\"' + src + '\" target=\"_blank\">' + '<img src=\"' + src + '\" class=\'container-image\'> </a>';
-
-      });
-
-      function clear(item) {
-        item.innerHTML = "";
-      }
-    } ";
-
-
-
-    // AJAX Request
-    var GiphyAJAXCall = new XMLHttpRequest();
-    GiphyAJAXCall.open('GET', url);
-    GiphyAJAXCall.send();
-
-    GiphyAJAXCall.addEventListener("load", function (e) {
-
-      var data = e.target.response;
-      pushToDOM(data);
-
-    });
-
-  }
-
-
-  /* 3. Show me the GIF */
-
-  function pushToDOM(input) {
-
-    var response = JSON.parse(input);
-    var imageUrls = response.data;
-    var container = document.querySelector(".js-container");
-    var result = document.querySelector(".results");
-
-    clear(container);
-    clear(result);
-
-    var imageUrls = response.data;
-
-    imageUrls.forEach(function (image) {
-
-      var src = image.images.fixed_height.url;
-
-      result.innerHTML = src.length + " GIFs found";
-      container.innerHTML += '<a href=\"' + src + '\" target=\"_blank\">' + '<img src=\"' + src + '\" class=\'container-image\'> </a>';
-
-    });
-
-    function clear(item) {
-      item.innerHTML = "";
-    }
-  } ";
-
-
-
-  // AJAX Request
-  var GiphyAJAXCall = new XMLHttpRequest();
-  GiphyAJAXCall.open('GET', url);
-  GiphyAJAXCall.send();
-
-  GiphyAJAXCall.addEventListener("load", function (e) {
-
-    var data = e.target.response;
-    pushToDOM(data);
-
-  });
-
 }
 
 
-/* 3. Show me the GIF */
+function searchGIFs() {
+  const searchQuery = document.getElementById("searchInput").value.trim();
+  if (!searchQuery) return; // Exit if search query is empty
 
-function pushToDOM(input) {
-
-  var response = JSON.parse(input);
-  var imageUrls = response.data;
-  var container = document.querySelector(".js-container");
-  var result = document.querySelector(".results");
-
-  clear(container);
-  clear(result);
-
-  var imageUrls = response.data;
-
-  imageUrls.forEach(function (image) {
-
-    var src = image.images.fixed_height.url;
-
-    result.innerHTML = src.length + " GIFs found";
-    container.innerHTML += '<a href=\"' + src + '\" target=\"_blank\">' + '<img src=\"' + src + '\" class=\'container-image\'> </a>';
-
-  });
-
-  function clear(item) {
-    item.innerHTML = "";
-  }
+  offset = 0; // Reset offset for new search
+  isLoading = true;
+  const gifContainer = document.getElementById("gifContainer");
+  gifContainer.innerHTML = ""; // 
+  fetchGIFs(searchQuery)
+    .then(gifs => {
+      displayGIFs(gifs);
+      isLoading = false;
+    })
+    .catch(error => {
+      console.error(error);
+      isLoading = false;
+    });
 }
+
+document.getElementById("searchInput").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    searchGIFs();
+  }
+});
+
+
+window.addEventListener("scroll", function () {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !isLoading) {
+    isLoading = true;
+    searchGIFs();
+  }
+});
+
+document.getElementById("searchButton").addEventListener("click", function () {
+  searchGIFs();
+});
